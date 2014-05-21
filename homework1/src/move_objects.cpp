330437243b4f91d2b988b3d1759c1d93e595d69f
Patrick Mania <pmania@tzi.de>
2014-05-21 22:54:11 +0200

    first working version. removed the static attribute from the sphere and box in the world

--- homework1/src/move_objects.cpp
+++ homework1/src/move_objects.cpp
@@ -1,43 +1,54 @@
 #include "gazebo/gazebo.hh"
+#include "gazebo/physics/physics.hh"
 
 namespace gazebo
 {
   class MoveObjects : public SystemPlugin
   {
     // Attributes
-    private: gazebo::physics::WorldPtr _world;
+    private: physics::WorldPtr _world;
+    private: event::ConnectionPtr updateConnection;
 
+    //////////////////////////////////////////////////
     public: virtual ~MoveObjects()
             {
             }
 
+    //////////////////////////////////////////////////
     public: virtual void Load(int _argc, char** _argv)
             {
-              std::cout << "FOOBAR" << std::endl;
-              // Get access to the world
-               // _world = gazebo::physics::get_world ("homework1" ); // maybe i must use "default" here, since this is the name in RDF model
-              // PhysicsEnginePtr physics_engine = gazebo::physics::World::GetPhysicsEngine ();
-              // std::cout << "The physics engine type:";
-              // std::cout << physics_engine->GetType() << std::endl;
-              // std::cout << "The name of the current world:";
-              // std::cout << _world->GetName() << std::endl;
-
+              // Don't do anything here, because the world will be loaded
+              // in Init()
             }
-// 
-//             // Called by the world update start event
-//     public: void OnUpdate(const common::UpdateInfo & /*_info*/)
-//             {
-//               // Get the box and sphere model
-//               // ModelPtr sphere_model = _world->getModel("sphere1");
-//               // ModelPtr box_model = _world->getModel("box1");
-//               
-//               // Apply a small linear velocity to the models
-//               // sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
-//               // box_model->SetLinearVel(math::Vector3(.03, 0, 0));
-//             }
 
+    /// Callback for every world update
+    //////////////////////////////////////////////////
+    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+            {
+               // Get the box and sphere model
+               physics::ModelPtr sphere_model = _world->GetModel("sphere1");
+               physics::ModelPtr box_model = _world->GetModel("box1");
+               
+               // Apply a small linear velocity to the models
+               sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
+               box_model->SetLinearVel(math::Vector3(.03, 0, 0));
+            }
+
+    //////////////////////////////////////////////////
     private: virtual void Init()
              {
+               // Get the first world in the simulation
+               _world = physics::get_world (); 
+              std::cout << "The name of the current world: ";
+              std::cout << _world->GetName() << std::endl;
+              physics::PhysicsEnginePtr physics_engine = _world->GetPhysicsEngine ();
+
+              std::cout << "The physics engine type: ";
+              std::cout << physics_engine->GetType() << std::endl;
+
+              // Subscribe to the World Update event
+              this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+                  boost::bind(&MoveObjects::OnUpdate, this, _1));
              }
   };
   // Register this plugin with the simulator
