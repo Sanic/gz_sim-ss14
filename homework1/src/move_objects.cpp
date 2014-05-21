334365c98d4ae68ffa22c9bbd8b9ccbcf23fcd86
Patrick Mania <pmania@tzi.de>
2014-05-21 23:03:32 +0200

    Refactoring - Seperated declaration and implementation

--- homework1/src/move_objects.cpp
+++ homework1/src/move_objects.cpp
@@ -1,56 +1,42 @@
 #include "gazebo/gazebo.hh"
 #include "gazebo/physics/physics.hh"
+#include "move_objects.hh"
 
 namespace gazebo
 {
-  class MoveObjects : public SystemPlugin
-  {
-    // Attributes
-    private: physics::WorldPtr _world;
-    private: event::ConnectionPtr updateConnection;
+    //////////////////////////////////////////////////
+    void MoveObjects::Load(int _argc, char** _argv)
+    {
+      // Don't do anything here, because the world will be loaded
+      // in Init()!
+    }
 
     //////////////////////////////////////////////////
-    public: virtual ~MoveObjects()
-            {
-            }
+    void MoveObjects::OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+       // Get the box and sphere model
+       physics::ModelPtr sphere_model = _world->GetModel("sphere1");
+       physics::ModelPtr box_model = _world->GetModel("box1");
+       
+       // Apply a small linear velocity to the models
+       sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
+       box_model->SetLinearVel(math::Vector3(.03, 0, 0));
+    }
 
     //////////////////////////////////////////////////
-    public: virtual void Load(int _argc, char** _argv)
-            {
-              // Don't do anything here, because the world will be loaded
-              // in Init()
-            }
+    void MoveObjects::Init()
+    {
+      // Get the first world in the simulation
+      _world = physics::get_world (); 
+     std::cout << "The name of the current world: ";
+     std::cout << _world->GetName() << std::endl;
+     physics::PhysicsEnginePtr physics_engine = _world->GetPhysicsEngine ();
 
-    /// Callback for every world update
-    //////////////////////////////////////////////////
-    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
-            {
-               // Get the box and sphere model
-               physics::ModelPtr sphere_model = _world->GetModel("sphere1");
-               physics::ModelPtr box_model = _world->GetModel("box1");
-               
-               // Apply a small linear velocity to the models
-               sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
-               box_model->SetLinearVel(math::Vector3(.03, 0, 0));
-            }
+     std::cout << "The physics engine type: ";
+     std::cout << physics_engine->GetType() << std::endl;
 
-    //////////////////////////////////////////////////
-    private: virtual void Init()
-             {
-               // Get the first world in the simulation
-               _world = physics::get_world (); 
-              std::cout << "The name of the current world: ";
-              std::cout << _world->GetName() << std::endl;
-              physics::PhysicsEnginePtr physics_engine = _world->GetPhysicsEngine ();
-
-              std::cout << "The physics engine type: ";
-              std::cout << physics_engine->GetType() << std::endl;
-
-              // Subscribe to the World Update event
-              this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-                  boost::bind(&MoveObjects::OnUpdate, this, _1));
-             }
-  };
-  // Register this plugin with the simulator
-  GZ_REGISTER_SYSTEM_PLUGIN(MoveObjects)
+     // Subscribe to the World Update event
+     this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+         boost::bind(&MoveObjects::OnUpdate, this, _1));
+    }
 }
