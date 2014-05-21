b5f08bf496955b5bb29c86c074579ed7d647505b
Patrick Mania <pmania@tzi.de>
2014-05-21 21:14:22 +0200

    Moved to ROS Hydro. You should then be able to comile this folder without any problems and load the plugin with gzserver -s

--- homework1/src/move_objects.cpp
+++ homework1/src/move_objects.cpp
@@ -5,14 +5,15 @@ namespace gazebo
   class MoveObjects : public SystemPlugin
   {
     // Attributes
-    private: WorldPtr _world;
+    private: gazebo::physics::WorldPtr _world;
 
     public: virtual ~MoveObjects()
             {
             }
 
-    public: virtual void Load(int _argc, char _argv)
+    public: virtual void Load(int _argc, char** _argv)
             {
+              std::cout << "FOOBAR" << std::endl;
               // Get access to the world
                // _world = gazebo::physics::get_world ("homework1" ); // maybe i must use "default" here, since this is the name in RDF model
               // PhysicsEnginePtr physics_engine = gazebo::physics::World::GetPhysicsEngine ();
@@ -22,18 +23,18 @@ namespace gazebo
               // std::cout << _world->GetName() << std::endl;
 
             }
-
-            // Called by the world update start event
-    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
-            {
-              // Get the box and sphere model
-              ModelPtr sphere_model = _world->getModel("sphere1");
-              ModelPtr box_model = _world->getModel("box1");
-              
-              // Apply a small linear velocity to the models
-              // sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
-              // box_model->SetLinearVel(math::Vector3(.03, 0, 0));
-            }
+// 
+//             // Called by the world update start event
+//     public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+//             {
+//               // Get the box and sphere model
+//               // ModelPtr sphere_model = _world->getModel("sphere1");
+//               // ModelPtr box_model = _world->getModel("box1");
+//               
+//               // Apply a small linear velocity to the models
+//               // sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
+//               // box_model->SetLinearVel(math::Vector3(.03, 0, 0));
+//             }
 
     private: virtual void Init()
              {
